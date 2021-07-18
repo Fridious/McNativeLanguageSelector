0f309540d6d3341666247058604e3b42b99104ee
Fridious <philippfriedhoff@outlook.de>
2021-07-18 14:25:23 +0200

    Updated license

--- src/main/java/org/mcnative/languageselector/McNativeLanguageSelectorPlugin.java
+++ src/main/java/org/mcnative/languageselector/McNativeLanguageSelectorPlugin.java
@@ -15,15 +15,15 @@ import org.mcnative.runtime.api.plugin.MinecraftPlugin;
 
 public class McNativeLanguageSelectorPlugin extends MinecraftPlugin {
 
-    public static final String RESOURCE_ID = "a120140d-bbdf-11eb-8ba0-0242ac180002";
-    public static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwcUY5PfVh6jLu7pP27cAtp95HwNpxfvE5rnv1ptBmYRUMIJS1vCutfjlEogfTMZL2aiKhdAKW4wuGNzzGPKxVcV/iVRiGtJPHRPpdvIgPkGVrglZVDrV8pcyhLd7L42erskdUZl9iAvXx3KZVp/5q/njRz0n9ZWGU7KHs4+ngpCRyFw1N1Kr/tZkqz+BgLXNT3Fxv2EU6qDpLNutsXkojmO+oMUsz8sFDPf38aFzn8lNNpFNTxdJgaaMEZN12G9WKj6XmKIdbq04Qcb7oX0BhugqPbvW0bULG+ija9aEOevdyFLrKrDT34qUmo8PmDPh1/FZmazsPTv1HIWkNhY1qwIDAQAB";
+    public static final String RESOURCE_ID = "415e443a-e655-11eb-8ba0-0242ac180002";
+    public static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApIr07y+zTUsWQgQvL3v4AZd8HtJtkpSlMDDqy6Jp81mGn83lfta17kFRTc2INZeCs7yN82pHOePmLhpcd9RJMx/GItDJGCmCnrVw2VFw6STv0OFBMZZj24C37v0/mvup8CRteMk3FKRcwItGqIuZAt/oRyxFanhfB856uGkYw1YPSbn0rCe0teQsCGbgIyjxO3D9gIOTp5Utf+fOmww7oxC4cf0D6ZOH97j5QrU1qt1sTfqnDJH+Xdzl5Z5IDgzYjDC7P0atMBiRPI8UeWnMsRu44TcWGKB0+xIozDmRBDClDhDcfZwqQG7naqMEPXmIa8n1RbBbTlaakEDSgsCeewIDAQAB";
 
     @Lifecycle(state = LifecycleState.LOAD)
     public void onLoad(LifecycleState state){
         getLogger().info("McNativeLanguageSelector is starting, please wait..");
 
         try{
-            //McNativeLicenseIntegration.newContext(this,RESOURCE_ID,PUBLIC_KEY).verifyOrCheckout();
+            McNativeLicenseIntegration.newContext(this,RESOURCE_ID,PUBLIC_KEY).verifyOrCheckout();
         }catch (LicenseNotValidException | CloudNotCheckoutLicenseException e){
             getLogger().error("--------------------------------");
             getLogger().error("-> Invalid license");
